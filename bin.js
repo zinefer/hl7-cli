26a0dc9690d857740322002c4b8e5a0628f9f8a2
zinefer <zinefer@gmail.com>
2022-07-22 15:42:18 -0600

    Bugfix: Correct package.json name
    Various readability improvements

--- bin.js
+++ bin.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var fs = require("fs");
+
 var DELIMETERS = {
     SEGMENT: "\n",
     FIELD: /(?<!\\)\|/,
@@ -8,39 +10,40 @@ var DELIMETERS = {
     SUBCOMPONENT: /(?<!\\)&/
 };
 
-var hl7 = {};
-
-var fs = require("fs");
-var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
-
 function forEachLeaf(obj, func) {
     if (obj.constructor === Array || obj.constructor == Object) {
         for (const k in obj) {
             obj[k] = forEachLeaf(obj[k], func);
         }
         return obj;
-    } else {
-        return func(obj);
-    }
+    }    
+    return func(obj);
 }
 
-var segments = stdinBuffer.toString().split(DELIMETERS.SEGMENT);
+var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
 
+var hl7 = {};
+
+var segments = stdinBuffer.toString().split(DELIMETERS.SEGMENT);
 segments.forEach(segment => {
     var name = segment.slice(0, 3);
     hl7[name] = segment.slice(4, -1);
 });
 
-var remainingDelimeters = [DELIMETERS.FIELD, DELIMETERS.FIELDREPEAT, DELIMETERS.COMPONENT, DELIMETERS.SUBCOMPONENT];
+var remainingDelimeters = [
+    DELIMETERS.FIELD,
+    DELIMETERS.FIELDREPEAT,
+    DELIMETERS.COMPONENT, 
+    DELIMETERS.SUBCOMPONENT
+];
 
 remainingDelimeters.forEach(delimeter => {    
-    forEachLeaf(hl7, (leaf) => {
+    forEachLeaf(hl7, leaf => {
         if (leaf.search(delimeter) != -1) {
             return leaf.split(delimeter);
         }
         return leaf;
     });
-
 });
 
 console.log(JSON.stringify(hl7, null, 2));
